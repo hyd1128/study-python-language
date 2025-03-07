a8fe84e35ea17464bdf4c6afbc46c330a9052acf
hyd <1357644645@qq.com>
2025-03-07 13:44:57 +0800

    提交最新

--- 99_草稿本.py
+++ 99_草稿本.py
@@ -1,2 +1,8 @@
-a = bool()
-print(a)
+def func_one():
+    def func_two():
+        print("aaaa")
+    func_two()
+    print("bbbb")
+
+
+func_one()
---
+++ python-standard-library/17_ZipFile/01_quickstart.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+
+# class Person:
+#
+#     def __init__(self, name, age, sex):
+#         self.name = name
+#         self.age = age
+#         self.sex = sex
+#
+#
+# if __name__ == '__main__':
+#     p1 = Person("小明", 18, "woman")
+#     p2 = Person("小华", 20, "men")
+
+
+a = 2
+
+
+def func1():
+    a = 10
+
+
+func1()
+
+print(a)
